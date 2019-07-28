2ed1e077ff89e8fd45c95659c7d6a941efb65bb0
aman tiwari <amantiwari12081999@gmail.com>
2019-07-28 19:38:43 +0530

    some changed;

--- src/components/home.js
+++ src/components/home.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   render(){
-    return <div className="container">
- 
+    return <div className="container shadow text-center ">
+
+     <div className='container shadow '>
+        <button className='btn btn-sm btn-primary  col-md'>18th Nov 2019</button>
+        <button className='btn btn-sm btn-primary  col-md'>19th Nov 2019</button>
+        <button className='btn btn-sm btn-primary  col-md'>20th Nov 2019</button>
+        <button className='btn btn-sm btn-primary  col-md'>21st Nov 2019</button>
+        <button className='btn btn-sm btn-primary  col-md'>22nd Nov 2019</button>
+     </div>
     </div>
   }
 }
