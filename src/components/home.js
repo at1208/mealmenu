2587c73146844ecf0e62195544e1927eff64bbbd
at1208 <amantiwari12081999@gmail.com>
2019-10-11 19:54:51 +0530

    react-stepper-horizonta added

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import Stepper from 'react-stepper-horizontal';
 
 class Home extends Component {
-  render(){
-    return <div className="container shadow text-center ">
 
-     <div className='container shadow '>
-        <button className='btn btn-sm btn-primary  col-md'>18th Nov 2019</button>
-        <button className='btn btn-sm btn-primary  col-md'>19th Nov 2019</button>
-        <button className='btn btn-sm btn-primary  col-md'>20th Nov 2019</button>
-        <button className='btn btn-sm btn-primary  col-md'>21st Nov 2019</button>
-        <button className='btn btn-sm btn-primary  col-md'>22nd Nov 2019</button>
-     </div>
+  state = {
+   
+  }
+
+  render(){
+    return <div className="container  text-center ">
+
+
+     <div>
+     <Stepper
+         steps={ [{title: '18th Nov 2019'}, {title: '19th Nov 2019'}, {title: '20th Nov 2019'}, {title: '21st Nov 2019'} , {title: '22nd Nov 2019'}] }
+         activeStep={0}
+         activeColor='#e53935'
+         completeColor='#ffee58'
+         activeTitleColor='#8d6e63'
+         completeTitleColor='#607d8b'
+         circleFontColor='#1a237e'
+         size={40}
+         circleFontSize={20}
+         titleFontSize={20}
+         circleTop={50}
+         titleTop={30}
+
+          />
+   </div>
+
     </div>
   }
 }
